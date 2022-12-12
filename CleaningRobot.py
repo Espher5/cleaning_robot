d22cfc44ac079b61fc7168baf8e33d8184e37f5e
Michelangelo Esposito <michelangeloesposito24@gmail.com>
2022-12-12 17:32:47 +0100

    updated template

--- CleaningRobot.py
+++ CleaningRobot.py
@@ -3,10 +3,10 @@ from CleaningRobotError import CleaningRobotError
 
 
 class CleaningRobot:
-    INFRARED_PIN =11
-    BATTERY_PIN = 12
-    RECHARGE_LED_PIN = 13
-    CLEANING_SYSTEM_PIN = 15
+    BATTERY_PIN = 11  # IBS pin
+    RECHARGE_LED_PIN = 12
+    CLEANING_SYSTEM_PIN = 13
+    INFRARED_PIN = 15
 
     # Wheel motor pins
     PWMA = 16
@@ -28,6 +28,11 @@ class CleaningRobot:
     FORWARD = 'f'
 
     def __init__(self, room_x: int, room_y: int):
+        """
+        Constructor
+        :param room_x: the x size of the room (i.e., how many cells are on the x-axis of the grid)
+        :parma room_y: the y size of the room (i.e., how many cells are on the y-axis of the grid)
+        """
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(self.INFRARED_PIN, GPIO.IN)
@@ -45,7 +50,7 @@ class CleaningRobot:
 
         self.room_x = room_x
         self.room_y = room_y
-        self.obstacles = []
+        self.obstacle = None
         self.pos_x = None
         self.pos_y = None
         self.facing = None
@@ -53,54 +58,41 @@ class CleaningRobot:
         self.battery_led_on = False
         self.cleaning_system_on = False
 
-    def robot_status(self) -> str:
-        """
-        Returns the current status of the robot, as well as any obstacle encountered
-        :return: the status of the robot as a string
-        """
-        status_string = '(' + str(self.pos_x) + ',' + str(self.pos_y) + ',' + self.facing + ')'
-        if len(self.obstacles) > 0:
-            for ob in self.obstacles:
-                status_string += ob
-
-        return status_string
-
-    def add_obstacle(self, ob_x: int, ob_y: int) -> None:
-        """
-        Adds an obstacles to the list of encountered obstacles
-        :param ob_x: the x coordinate of the obstacle
-        :param ob_y: the y coordinate of the obstacle
-        """
-        self.obstacles.append('(' + str(ob_x) + ',' + str(ob_y) + ')')
-
     def initialize_robot(self) -> None:
         """
         Initializes the robot in the starting position (0,0,N)
         """
         pass
 
-    def execute_command(self, command_string: str) -> str:
+    def robot_status(self) -> str:
+        """
+        Returns the current status of the robot, as well as any obstacle encountered
+        :return: the status of the robot as a string
+        """
+        pass
+
+    def execute_command(self, command: str) -> str:
         """
         It makes the robot move inside the room according to a command string received by the RMS.
-        The return string contains the new position of the rover, its direction, and the obstacles it has
-         encountered while moving in the planet (if any).
-        :param command_string: the string containing the list of commands to be performed by the robot.
-            The commands can be:
+        The return string contains the new position of the robot, its direction, and the obstacles it has
+        encountered while moving in the room (if any).
+        :param command: the string containing the command corresponding to the action the robot needs to perform.
+            The command can be:
             - f for moving forward one cell in the room.
             - r for turning right 90 degrees.
             - l for turning left 90 degrees.
-        :return: The return string that contains the updated position and direction of the
-            rover, and the obstacles the rover has encountered while moving inside the room (if any).
+        :return: The string that contains the updated position and direction of the
+            robot, and the obstacle the rover has encountered while moving inside the room (if any).
             The return string (without white spaces) has the following format:
-            "(x,y,dir)(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)".
+            "(x,y,dir)(o_x,o_y)".
             x and y define the new position of the rover while dir represents its direction (i.e., N, S, W, or E).
-            Finally, oi_x and oi_y are the coordinates of the i-th encountered obstacle.
+            Finally, o_x and o_y are the coordinates of the encountered obstacle.
         """
         pass
 
     def obstacle_found(self) -> bool:
         """
-        Checks whether the infrared distance sensor detects an obstacle in front of it.
+        Checks whether the infrared distance sensor has detected an obstacle in front of it.
         :return: True if the infrared sensor detects something, False otherwise.
         """
 
